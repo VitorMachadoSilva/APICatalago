17c93c6032ce191ab391e1faf731f6b7dd72489b
VitorMachadoSilva <vitor.machado.silva05@gmail.com>
2024-09-15 20:05:25 -0300

    Add files via upload
    API Curso Udemy

---
+++ APICatalago/APICatalago/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalago.Models;
+//só possui propriedades (classe anêmica)
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
